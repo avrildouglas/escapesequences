b87daf9f4b8e7244e9a1ed02e4c4ccc5243035f3
student <avrildouglas>
2017-06-02 17:38:39 -0400

    second commit

---
+++ src/escape/EscapeTheSequences.java
@@ -0,0 +1,29 @@
+package escape;
+
+public class EscapeTheSequences {
+	 public static void main(String[] args) 
+	 { 
+	    System.out.print("\t");
+	    System.out.print("*********\r\t\n\t");
+	    System.out.print("*\t*\r\t\r\t");
+	    System.out.print("*\t*\r\t\r\t");
+	    System.out.print("*\t*\r\t\r\t");
+	    System.out.print("*\t*\r\n\r\t");
+	    System.out.print("*********");
+	    System.out.print("\r\n\r\n\r\n");
+	    System.out.print("\t");
+	    System.out.print("   ***\r\t\n\t");
+	    System.out.print("  *   *\r\t\n\t");
+	    System.out.print(" *     *\r\t\n\t");
+	    System.out.print(" *     *\r\t\n\t");
+	    System.out.print("  *   *\r\t\n\t");
+	    System.out.print("   ***\r\t\n\t");
+	    System.out.print("\r\n");
+	    System.out.print("\t");
+	    System.out.print("    *\r\t\n\t");
+	    System.out.print("   * *\r\t\n\t");
+	    System.out.print("  *   *\r\t\n\t");
+	    System.out.print("   * *\r\t\n\t");
+	    System.out.print("    *\r");
+	 }
+}
